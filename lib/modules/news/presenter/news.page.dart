79d7df5c6aa97298f574a2ae715d02719c24a0a5
Felipe Ribeiro <feliper.dev@gmail.com>
2021-10-05 20:52:36 -0300

    Adicionando TabBar na página de notícias; Personalizando container que exibirá a notícia

--- lib/modules/news/presenter/news.page.dart
+++ lib/modules/news/presenter/news.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/modules/news/presenter/widgets/news_container.dart';
 import 'package:news_app/shared/theme/colors.dart';
 
 class NewsPage extends StatelessWidget {
@@ -6,9 +7,43 @@ class NewsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.mainColor,
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {},
+          ),
+          centerTitle: true,
+          title: const Text('News APP', style: TextStyle(color: Colors.white)),
+          backgroundColor: AppColors.mainColor,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.text_snippet_outlined),
+                text: 'Notícias',
+              ),
+              Tab(
+                icon: Icon(Icons.event),
+                text: 'Eventos',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              itemCount: 10,
+              itemBuilder: (_, index) {
+                return NewsContainer();
+              },
+            ),
+            Container(),
+          ],
+        ),
       ),
     );
   }
