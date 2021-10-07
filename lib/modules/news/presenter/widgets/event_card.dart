8694def7c8f4307c1c2ec16656c31bdedd857176
Felipe Ribeiro <feliper.dev@gmail.com>
2021-10-07 09:48:37 -0300

    Adicionando um pouco mais de estilização aos cards de notícia

---
+++ lib/modules/news/presenter/widgets/event_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventCard extends StatelessWidget {
+  const EventCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
